37f2afb7a6c14700fe26b0dc257455c616bea437
sailtech21 <sailtech21@gmail.com>
2026-08-20 12:58:01 +0200

    Disable swcMinify and set single-thread mode for shared hosting environments

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" }
     ]
   },
-  experimental: { serverActions: { bodySizeLimit: "10mb" } }
+  experimental: {
+    serverActions: { bodySizeLimit: "10mb" },
+    cpus: 1,
+    workerThreads: false
+  },
+  webpack: (config) => {
+    config.parallelism = 1;
+    return config;
+  }
 };
 module.exports = nextConfig;
